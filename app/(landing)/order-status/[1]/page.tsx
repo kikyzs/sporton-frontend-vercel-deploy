ffd6f5fdf8887f180b757366085043020be165e4
kikyzs <2310203@student.itsk-soepraoen.ac.id>
2026-02-08 14:17:58 +0700

    Live Session 2

--- app/(landing)/order-status/[1]/page.tsx
+++ app/(landing)/order-status/[1]/page.tsx
@@ -7,7 +7,7 @@ import OrderSubmitted from "../../components/order-status/order-submitted";
 const OrderStatus = () => {
   const [isConfirmed, setIsConfirmed] = useState(false);
   return (
-    <main className="bg-gray-100 min-h-[80vh]">
+    <main className="bg-gray-100 min-h-[100vh]">
       <div className="max-w-5xl mx-auto py-20">
         <h1 className="font-bold text-5xl text-center mb-11">Order Status</h1>
       </div>
